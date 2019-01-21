54870d0efd5dea90eceb7116d16cd4ad74d80074
wangbingning <bingning.wang@nlpr.ia.ac.cn>
2019-01-21 16:13:41 +0800

    transformer model

---
+++ Transformer/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+ @Time    : 2019/1/21 下午4:12
+ @FileName: __init__.py.py
+ @author: 王炳宁
+ @contact: wangbingning@sogou-inc.com
+"""
---
+++ Transformer/model.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+ @Time    : 2019/1/21 下午4:12
+ @FileName: model.py
+ @author: 王炳宁
+ @contact: wangbingning@sogou-inc.com
+"""
+
